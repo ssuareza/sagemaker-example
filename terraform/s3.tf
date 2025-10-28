d544532c36bcea6866e73161661865f062de7ea3
Sebastian Suarez <ssuareza@pm.me>
2025-10-28 13:46:51 +0100

    Update

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "sagemaker_example" {
-  bucket = "sagemaker-example"
+  bucket = "sagemaker-example-${random_id.suffix.hex}"
 }
 
 resource "aws_s3_bucket_acl" "sagemaker_example_acl" {
@@ -13,4 +13,9 @@ resource "aws_s3_bucket_public_access_block" "sagemaker_example_public_access_bl
   block_public_acls       = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+# random id to create a bucket name
+resource "random_id" "suffix" {
+  byte_length = 4
 }
